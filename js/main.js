b9846a9be09f4530bbda353959e86763345dbaa8
romancudev <roman.g@cudev.org>
2018-03-07 01:03:48 +0200

    1.1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 (function() {
 	var board = document.getElementById('board');
 	var resultContainer = document.getElementById('text');
-	var totalMovesCounter, currentPlayer, state;
+	var startGameButton = document.getElementById('start-game');
+	var totalMovesCounter, currentPlayer;
+	
+	var state = {
+		'X': {
+			'totalMoves': 0,
+			'doneMoves': []
+		},
+		'O': {
+			'totalMoves': 0,
+			'doneMoves': []
+		}
+	};
 
 	const winCombos = [
 		[1, 2, 3],
@@ -78,6 +90,9 @@
 
 	function updateHistory() {
 		currentPlayer = ((currentPlayer === 'X') ? 'O' : 'X');
+
+		var getParameters = `?totalMovesCounter=${totalMovesCounter}&playerX=${state['X'].doneMoves}&playerO=${state['O'].doneMoves}&currentPlayer=${currentPlayer}`;
+		window.history.pushState(null, null, `${window.location.pathname}${getParameters}`);
 	}
 
 	Array.prototype.constaintCombination = function(combosArray) {
@@ -86,23 +101,32 @@
     	}, this);
 	}
 
-	document.getElementById('start-game').addEventListener('click', startGame);
-
 	function setDefaultOptions() {
-		totalMovesCounter = 1;
+		totalMovesCounter = findGet('totalMovesCounter') ? findGet('totalMovesCounter') : 1;
 		
-		currentPlayer = 'X';
+		currentPlayer = findGet('currentPlayer') ? findGet('currentPlayer') : 'X';
 		
-		state = {
-			'X': {
-				'totalMoves': 0,
-				'doneMoves': []
-			},
-			'O': {
-				'totalMoves': 0,
-				'doneMoves': []
-			}
-		};
+		state.X.totalMoves = findGet('playerX') ? findGet('playerX') : 0;
+		state.O.totalMoves = findGet('playerY') ? findGet('playerY') : 0;
+		state.X.doneMoves = state.O.doneMoves = [];
 	}
+
+	function findGet(parameter) {
+		parameter =  parameter.replace(/[\[\]]/g, "\\$&");
+		var regex = new RegExp("[?&]" + parameter + "(=([^&#]*)|&|#|$)");
+		
+		var results = regex.exec(window.location.href);
+		if (!results) {
+			return null;
+		}
+		if (!results[2]) {
+			return '';
+		}
+
+		return decodeURIComponent(results[2].replace(/\+/g, " "));
+	}
+
+	startGameButton.innerText = (window.location.search.toString().length > 0 ? 'Continue game' : 'Start new game');
+	startGameButton.addEventListener('click', startGame);
 
 })();
